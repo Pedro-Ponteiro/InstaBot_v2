24b84cb84ce58d056dd83cef76228d6f2b8b43d0
Pedro-Ponteiro <pedromcsp@outlook.com>
2021-10-23 21:16:23 -0300

    [feat] added function to get the private info secret

--- InstaBot/env_secrets.py
+++ InstaBot/env_secrets.py
@@ -1,7 +1,19 @@
 import getpass
-from typing import Tuple
+from typing import List, Tuple
 
 
 def get_login_pwd() -> Tuple[str, str]:
     print("Credentials")
     return (input("Username: "), getpass.getpass("Password: "))
+
+
+def get_dont_like() -> List[str]:
+
+    words = [
+        "hot",
+        "india",
+        "girl",
+        "guy",
+    ]
+
+    return words + [w.title() for w in words] + [w.upper() for w in words]
